732eeaadaf8a9a3701c902987d0f82379bc28710
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2024-04-11 10:52:43 +0200

    feat: added screenshots, added workbox config file

--- app/routes/manifest[.]webmanifest.ts
+++ app/routes/manifest[.]webmanifest.ts
@@ -40,7 +40,64 @@ const manifest = {
       purpose: "any",
     },
   ],
-  screenshots: [],
+  screenshots: [
+    {
+      src: "/screenshots/screen_desktop_1_dark.jpg",
+      sizes: "1066x600",
+      type: "image/jpeg",
+      form_factor: "wide",
+      label: "Index page on desktop in dark mode",
+    },
+    {
+      src: "/screenshots/screen_desktop_1_light.jpg",
+      sizes: "1066x600",
+      type: "image/jpeg",
+      form_factor: "wide",
+      label: "Index page on desktop in light mode",
+    },
+    {
+      src: "/screenshots/screen_mobile_1_dark.jpg",
+      sizes: "337x600",
+      type: "image/jpeg",
+      form_factor: "narrow",
+      label: "Index page on mobile without route data in dark mode",
+    },
+    {
+      src: "/screenshots/screen_mobile_1_light.jpg",
+      sizes: "337x600",
+      type: "image/jpeg",
+      form_factor: "narrow",
+      label: "Index page on mobile without route data in light mode",
+    },
+    {
+      src: "/screenshots/screen_mobile_2_dark.jpg",
+      sizes: "337x600",
+      type: "image/jpeg",
+      form_factor: "narrow",
+      label: "Index page on mobile with route data in dark mode",
+    },
+    {
+      src: "/screenshots/screen_mobile_2_light.jpg",
+      sizes: "337x600",
+      type: "image/jpeg",
+      form_factor: "narrow",
+      label: "Index page on mobile with route data in light mode",
+    },
+    {
+      src: "/screenshots/screen_mobile_3_dark.jpg",
+      sizes: "337x600",
+      type: "image/jpeg",
+      form_factor: "narrow",
+      label: "Index page on mobile with aircraft data in dark mode",
+    },
+    {
+      src: "/screenshots/screen_mobile_3_light.jpg",
+      sizes: "337x600",
+      type: "image/jpeg",
+      form_factor: "narrow",
+      label: "Index page on mobile with aircraft data in light mode",
+    },
+  ],
 };
 
 export const loader: LoaderFunction = () => json(manifest);
